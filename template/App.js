07bd220b6e40702ebb11c42da2188c4f57cfda06
idegtyarev <cum_butt@mail.ru>
2022-07-19 20:28:40 +0300

    initial comit

--- template/App.js
+++ template/App.js
@@ -1,42 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {GetDataFromStorage, SetDataToStorage, AppLife, GetDataFromServer} from './Tools.js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useEffect, useState} from "react";
-import { WebView } from 'react-native-webview';
-import { Storage } from 'expo-storage';
-import * as constants from "./Constants";
-import axios from "axios";
+import React, { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
-export default function App() {    
-    let [current_link, setData] = useState(constants.block_banner);
-    const debug = true;
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
 
-        useEffect(() => {
-        const launch = async () => {
-            const current_link_ = await AppLife();
-            setData(current_link_);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-        }
-        launch();
-    }, [])
+    prepare();
+  }, []);
 
-    console.log('current_link=', current_link);
-    
-    //return (<Text>TEST TEXT</Text>);
-    
-    return (
-      <WebView 
-        style={styles.container}
-        source={{ uri: current_link }}/>
-    );
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+      <Entypo name="rocket" size={30} />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
